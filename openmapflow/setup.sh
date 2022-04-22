0125289acc05c3af39edf34f8661abed34112ed1
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-04-22 13:52:54 -0400

    Initialize dvc in example

--- openmapflow/setup.sh
+++ openmapflow/setup.sh
@@ -3,5 +3,20 @@
 #- container registry creation
 # env creation
 
-#- download example labels, features, models
-#- dvc init things
+
+# If project is directly in git repo
+# dvc init
+
+# If project is in subdirectory
+# cd <subdir> dvc init --subdir
+
+# Make data directories: [models, features, processed_labels, raw_labels]
+
+# Add dvc data: [models, features, processed_labels, raw_labels]
+# dvc add data/raw_labels ...
+# dvc commit
+
+# Set dvc remote, Google Drive by default for simplicity
+# https://dvc.org/doc/user-guide/setup-google-drive-remote
+
+# dvc remote add -d gdrive gdrive://1EMHILcNFwdusMHHs4eC4OVIJ0Ncp9fiY/crop-mask-example-dvc-store
