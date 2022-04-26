9fe08b32a67bdecbca9ea7327bd91b5f1ce64758
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-04-26 02:03:00 -0400

    Add more imports

--- openmapflow/__init__.py
+++ openmapflow/__init__.py
@@ -1,3 +1,11 @@
 from openmapflow.main import get_training_data, create_features, push_new_training_data
+from openmapflow.dataset import LabeledDataset
+from openmapflow.processor import Processor
 
-__all__ = ["get_training_data", "create_features", "push_new_training_data"]
+__all__ = [
+    "get_training_data",
+    "create_features",
+    "push_new_training_data",
+    "LabeledDataset",
+    "Processor",
+]
