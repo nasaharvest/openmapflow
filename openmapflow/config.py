daf949cb347be6bf085cdd1c6a4670ad707df78c
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-02 19:38:19 -0400

    Config for google cloud

--- openmapflow/config.py
+++ openmapflow/config.py
@@ -1,16 +1,43 @@
 import yaml
+from pathlib import Path
 from .utils import find_project_root
 
+# -------------- Load configuration -------------------------------------------
 PROJECT_ROOT = find_project_root(["openmapflow.yaml", "data"])
 
 with (PROJECT_ROOT / "openmapflow.yaml").open() as f:
     CONFIG_YML = yaml.safe_load(f)
 
-RELATIVE_PATHS = {k: f"data/{v}" for k, v in CONFIG_YML["data_paths"].items()}
-FULL_PATHS = {k: PROJECT_ROOT / v for k, v in RELATIVE_PATHS.items()}
-TIF_BUCKET_NAME = CONFIG_YML["labeled_tifs_bucket"]
+PROJECT = CONFIG_YML["project"]["id"]
 
-# df column names
+# -------------- PATHS --------------------------------------------------------
+RELATIVE_PATHS = {k: f"data/{v}" for k, v in CONFIG_YML["data"]["paths"].items()}
+FULL_PATHS = {k: PROJECT_ROOT / v for k, v in RELATIVE_PATHS.items()}
+OPENMAPFLOW_DIR = Path(__file__).parent
+
+
+# -------------- GCLOUD -------------------------------------------------------
+GCLOUD_PROJECT_ID = CONFIG_YML["gcloud"]["project_id"]
+GCLOUD_LOCATION = CONFIG_YML["gcloud"]["location"]
+TIF_BUCKET_NAME = CONFIG_YML["gcloud"]["buckets"]["labeled_tifs"]
+DOCKER_TAG = f"{GCLOUD_LOCATION}-docker.pkg.dev/{GCLOUD_PROJECT_ID}/{PROJECT}/{PROJECT}"
+
+# -------------- Helper functions ---------------------------------------------
+def get_model_names_as_str():
+    return " ".join([p.stem for p in Path(FULL_PATHS["models"]).glob("*.pt")])
+
+
+def deploy_env_variables():
+    env_variables = f"PROJECT={PROJECT} "
+    env_variables += f"MODELS_DIR={RELATIVE_PATHS['models']} "
+    env_variables += f"OPENMAPFLOW_DIR={OPENMAPFLOW_DIR} "
+    env_variables += f"GCLOUD_PROJECT_ID={GCLOUD_PROJECT_ID} "
+    env_variables += f"GCLOUD_LOCATION={GCLOUD_LOCATION} "
+    env_variables += f"TAG={DOCKER_TAG} "
+    return env_variables
+
+
+# -------------- Dataframe column names ---------------------------------------
 SOURCE = "source"
 CLASS_PROB = "class_probability"
 START = "start_date"
@@ -28,6 +55,7 @@ TIF_PATHS = "tif_paths"
 LABELER_NAMES = "email"
 LABEL_DUR = "analysis_duration"
 
+# -------------- Months -------------------------------------------------------
 MONTHS = [
     "January",
     "February",
