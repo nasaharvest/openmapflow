e47591ecf0b074c9b30c5fc9bd8698f62834f718
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-07 16:56:31 -0400

    Remove need for pandas import in config

--- openmapflow/utils.py
+++ openmapflow/utils.py
@@ -9,16 +9,3 @@ def try_txt_read(file_path: Path) -> List[str]:
         return pd.read_csv(file_path, sep="\n", header=None)[0].tolist()
     except FileNotFoundError:
         return []
-
-
-def find_project_root(files_to_check: List[str]) -> Path:
-    """
-    Find the project root directory by checking for existence of certain files
-    """
-    possible_roots = [Path.cwd(), Path.cwd().parent]
-
-    for root in possible_roots:
-        if all([(root / c).exists() for c in files_to_check]):
-            return root
-
-    raise FileExistsError(f"{files_to_check} not found in {possible_roots}.")
