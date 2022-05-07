9751c3f7a0b246c44b369a6f8f567a43fada46e1
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-07 16:57:55 -0400

    Successfully deploy all resources

--- crop-mask-example/deploy.sh
+++ crop-mask-example/deploy.sh
@@ -1,65 +1,91 @@
+#####################################################################################################
+# This script deploys all the resources necessary to make maps on Google Cloud with OpenMapFlow
+#####################################################################################################
 
-# exit when any command fails
-set -e
+set -e # Exit when any command fails
 
+echo "1/7 Setting OpenMapFlow environment variables"
 export $(
         python -c \
         "from openmapflow.config import deploy_env_variables; \
         print(deploy_env_variables())"
         )
-echo "PROJECT: $PROJECT"
-echo "MODELS_DIR: $MODELS_DIR"
-echo "GCLOUD_PROJECT: $GCLOUD_PROJECT"
-echo "GCLOUD_LOCATION: $GCLOUD_LOCATION"
-echo "TAG: $TAG"
-echo "OPENMAPFLOW_DIR: $OPENMAPFLOW_DIR"
 
-# Ensure the models on DVC are being deployed
-dvc pull $MODELS_DIR.dvc
+env | grep OPENMAPFLOW
 
-export MODELS=$(
+
+echo "2/7 Ensuring latest models are available for deployment"
+dvc pull $OPENMAPFLOW_MODELS_DIR.dvc
+
+export OPENMAPFLOW_MODELS=$(
         python -c \
         "from openmapflow.config import get_model_names_as_str; \
         print(get_model_names_as_str())"
 )
-echo "MODELS: $MODELS"
+echo "MODELS: $OPENMAPFLOW_MODELS"
 
-# Enable artifact registry
+
+echo "3/7 Create Google Cloud Buckets if they don't exist"
+for BUCKET in $OPENMAPFLOW_GCLOUD_BUCKET_LABELED_TIFS \
+        $OPENMAPFLOW_GCLOUD_BUCKET_INFERENCE_TIFS \
+        $OPENMAPFLOW_GCLOUD_BUCKET_PREDS \
+        $OPENMAPFLOW_GCLOUD_BUCKET_PREDS_MERGED
+do
+        if [ "$(gsutil ls -b gs://$BUCKET)" ]; then
+                echo "gs://$BUCKET already exists"
+        else
+                gsutil mb -l $OPENMAPFLOW_GCLOUD_LOCATION gs://$BUCKET
+        fi
+done
+
+
+echo "4/7 Checking if Artifact Registry needs to be created for storing OpenMapFlow docker images"
 gcloud services enable artifactregistry.googleapis.com
-
-if [ -z "$(gcloud artifacts repositories list --format='get(name)' --filter $PROJECT)" ]; then
-        gcloud artifacts repositories create $PROJECT --location $GCLOUD_LOCATION --repository-format docker
+if [ -z "$(gcloud artifacts repositories list --format='get(name)' --filter $OPENMAPFLOW_PROJECT)" ]; then
+        gcloud artifacts repositories create $OPENMAPFLOW_PROJECT \
+        --location $OPENMAPFLOW_GCLOUD_LOCATION \
+        --repository-format docker
 fi
 
+echo "5/7 Build and push inference docker image to Google Cloud artifact registry"
 docker build . \
-        -f $OPENMAPFLOW_DIR/Dockerfile \
-        --build-arg MODELS="$MODELS" \
-        --build-arg MODELS_DIR="$MODELS_DIR" \
-        -t $TAG
+        -f $OPENMAPFLOW_LIBRARY_DIR/Dockerfile \
+        --build-arg MODELS="$OPENMAPFLOW_MODELS" \
+        --build-arg MODELS_DIR="$OPENMAPFLOW_MODELS_DIR" \
+        --build-arg DEST_BUCKET="$OPENMAPFLOW_GCLOUD_BUCKET_PREDS" \
+        -t $OPENMAPFLOW_DOCKER_TAG
 
-docker push $TAG
+docker push $OPENMAPFLOW_DOCKER_TAG
 
-gcloud run deploy $PROJECT --image $TAG:latest \
+
+echo "6/7 Deploy inference docker image to Google Cloud Run"
+echo "Deploying prediction server on port 8080"
+gcloud run deploy $OPENMAPFLOW_PROJECT --image $OPENMAPFLOW_DOCKER_TAG:latest \
         --cpu=4 \
         --memory=8Gi \
         --platform=managed \
-        --region=$GCLOUD_LOCATION \
+        --region=$OPENMAPFLOW_GCLOUD_LOCATION \
         --allow-unauthenticated \
         --concurrency 10 \
         --port 8080
 
-gcloud run deploy $PROJECT-management-api --image $TAG:latest \
+echo "Deploying model list server on port 8081"
+gcloud run deploy $OPENMAPFLOW_PROJECT-management-api --image $OPENMAPFLOW_DOCKER_TAG:latest \
         --memory=4Gi \
         --platform=managed \
-        --region=$GCLOUD_LOCATION \
+        --region=$OPENMAPFLOW_GCLOUD_LOCATION \
         --allow-unauthenticated \
         --port 8081
 
-# gcloud functions deploy trigger-inference \
-#     --source=src/trigger_inference_function \
-#     --trigger-bucket=$BUCKET \
-#     --allow-unauthenticated \
-#     --runtime=python39 \
-#     --entry-point=trigger \
-#     --set-env-vars MODELS="$MODELS",INFERENCE_HOST="$URL" \
-#     --timeout=300s
+
+echo "7. Deploy inference trigger as a Google Cloud Function"
+export OPENMAPLFOW_URL=$(gcloud run services list --platform managed --filter $OPENMAPFLOW_PROJECT --limit 1 --format='get(URL)')
+
+gcloud functions deploy trigger-inference \
+    --source=$OPENMAPFLOW_LIBRARY_DIR/trigger_inference_function \
+    --trigger-bucket=$OPENMAPFLOW_GCLOUD_BUCKET_INFERENCE_TIFS \
+    --allow-unauthenticated \
+    --runtime=python39 \
+    --entry-point=trigger \
+    --set-env-vars MODELS="$OPENMAPFLOW_MODELS",INFERENCE_HOST="$OPENMAPFLOW_URL" \
+    --timeout=300s
