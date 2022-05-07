1bf5e1f8440078d23c3f8645ec301c0b6e57e2be
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-07 16:54:41 -0400

    Make config strictly related to yaml config

--- openmapflow/config.py
+++ openmapflow/config.py
@@ -1,6 +1,20 @@
 import yaml
 from pathlib import Path
-from .utils import find_project_root
+from typing import List
+
+
+def find_project_root(files_to_check: List[str]) -> Path:
+    """
+    Find the project root directory by checking for existence of certain files
+    """
+    possible_roots = [Path.cwd(), Path.cwd().parent]
+
+    for root in possible_roots:
+        if all([(root / c).exists() for c in files_to_check]):
+            return root
+
+    raise FileExistsError(f"{files_to_check} not found in {possible_roots}.")
+
 
 # -------------- Load configuration -------------------------------------------
 PROJECT_ROOT = find_project_root(["openmapflow.yaml", "data"])
@@ -13,61 +27,39 @@ PROJECT = CONFIG_YML["project"]["id"]
 # -------------- PATHS --------------------------------------------------------
 RELATIVE_PATHS = {k: f"data/{v}" for k, v in CONFIG_YML["data"]["paths"].items()}
 FULL_PATHS = {k: PROJECT_ROOT / v for k, v in RELATIVE_PATHS.items()}
-OPENMAPFLOW_DIR = Path(__file__).parent
+LIBRARY_DIR = Path(__file__).parent
 
 
 # -------------- GCLOUD -------------------------------------------------------
 GCLOUD_PROJECT_ID = CONFIG_YML["gcloud"]["project_id"]
 GCLOUD_LOCATION = CONFIG_YML["gcloud"]["location"]
-TIF_BUCKET_NAME = CONFIG_YML["gcloud"]["buckets"]["labeled_tifs"]
+
+GCLOUD_BUCKET_LABELED_TIFS = CONFIG_YML["gcloud"]["buckets"]["labeled_tifs"]
+GCLOUD_BUCKET_INFERENCE_TIFS = CONFIG_YML["gcloud"]["buckets"]["inference_tifs"]
+GCLOUD_BUCKET_PREDS = CONFIG_YML["gcloud"]["buckets"]["preds"]
+GCLOUD_BUCKET_PREDS_MERGED = CONFIG_YML["gcloud"]["buckets"]["preds_merged"]
+
 DOCKER_TAG = f"{GCLOUD_LOCATION}-docker.pkg.dev/{GCLOUD_PROJECT_ID}/{PROJECT}/{PROJECT}"
 
 
 # -------------- Helper functions ---------------------------------------------
-def get_model_names_as_str():
+def get_model_names_as_str() -> str:
     return " ".join([p.stem for p in Path(FULL_PATHS["models"]).glob("*.pt")])
 
 
-def deploy_env_variables():
-    env_variables = f"PROJECT={PROJECT} "
-    env_variables += f"MODELS_DIR={RELATIVE_PATHS['models']} "
-    env_variables += f"OPENMAPFLOW_DIR={OPENMAPFLOW_DIR} "
-    env_variables += f"GCLOUD_PROJECT_ID={GCLOUD_PROJECT_ID} "
-    env_variables += f"GCLOUD_LOCATION={GCLOUD_LOCATION} "
-    env_variables += f"TAG={DOCKER_TAG} "
+def deploy_env_variables() -> str:
+    prefix = "OPENMAPFLOW"
+    deploy_env_dict = {
+        "PROJECT": PROJECT,
+        "MODELS_DIR": RELATIVE_PATHS["models"],
+        "LIBRARY_DIR": LIBRARY_DIR,
+        "GCLOUD_PROJECT_ID": GCLOUD_PROJECT_ID,
+        "GCLOUD_LOCATION": GCLOUD_LOCATION,
+        "GCLOUD_BUCKET_LABELED_TIFS": GCLOUD_BUCKET_LABELED_TIFS,
+        "GCLOUD_BUCKET_INFERENCE_TIFS": GCLOUD_BUCKET_INFERENCE_TIFS,
+        "GCLOUD_BUCKET_PREDS": GCLOUD_BUCKET_PREDS,
+        "GCLOUD_BUCKET_PREDS_MERGED": GCLOUD_BUCKET_PREDS_MERGED,
+        "DOCKER_TAG": DOCKER_TAG,
+    }
+    env_variables = " ".join([f"{prefix}_{k}={v}" for k, v in deploy_env_dict.items()])
     return env_variables
-
-
-# -------------- Dataframe column names ---------------------------------------
-SOURCE = "source"
-CLASS_PROB = "class_probability"
-START = "start_date"
-END = "end_date"
-LON = "lon"
-LAT = "lat"
-COUNTRY = "country"
-NUM_LABELERS = "num_labelers"
-SUBSET = "subset"
-DATASET = "dataset"
-ALREADY_EXISTS = "already_exists"
-FEATURE_FILENAME = "filename"
-FEATURE_PATH = "save_path"
-TIF_PATHS = "tif_paths"
-LABELER_NAMES = "email"
-LABEL_DUR = "analysis_duration"
-
-# -------------- Months -------------------------------------------------------
-MONTHS = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-]
