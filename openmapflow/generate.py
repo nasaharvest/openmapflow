acb966deb51a1d2975d81b871ce0f12c23401e92
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-10 02:31:09 -0400

    Generate script v0

--- openmapflow/generate.py
+++ openmapflow/generate.py
@@ -1,59 +1,93 @@
 import argparse
+import shutil
+import tarfile
+
+from pathlib import Path
+from openmapflow.constants import CONFIG_FILE
 
 parser = argparse.ArgumentParser(description="Generate OpenMapFlow project.")
-parser.add_argument("name", type=str, help="Project name")
-parser.add_argument("description", type=str, help="Project description", default="")
+parser.add_argument("--name", type=str, help="Project name", default=Path.cwd().name)
+parser.add_argument("--description", type=str, help="Project description", default="")
 parser.add_argument(
-    "gcloud_project_id", type=str, help="Google Cloud Project ID", default=""
-)
-parser.add_argument(
-    "gcloud_location", type=str, help="Google Cloud Location", default="us-central-1"
-)
-parser.add_argument(
-    "gcloud_bucket_labeled_tifs",
+    "--gcloud_project_id",
     type=str,
-    help="Google Cloud Bucket for labeled tifs",
+    help="Google Cloud Project ID",
+    default="",
+)
+parser.add_argument(
+    "--gcloud_location",
+    type=str,
+    help="Google Cloud Location (default: us-central-1)",
+    default="us-central-1",
+)
+parser.add_argument(
+    "--gcloud_bucket_labeled_tifs",
+    type=str,
+    help="Google Cloud Bucket for labeled tifs (default: crop-mask-tifs2)",
     default="crop-mask-tifs2",
 )
-
-
+parser.add_argument("--force", action="store_true", help="Force overwrite")
 args = parser.parse_args()
 
-openmapflow_dict = {
-    "version": "0.0.1",
-    "project": args.name,
-    "description": args.description,
-    "gcloud": {
-        "project_id": args.gcloud_project_id,
-        "location": args.gcloud_location,
-        "bucket_labeled_tifs": args.gcloud_bucket_labeled_tifs,
-    },
-}
+
+def allow_write(p):
+    if args.force or not Path(p).exists():
+        return True
+    overwrite = input(f"{str(p)} already exists. Overwrite? (y/n): ")
+    return overwrite.lower() == "y"
 
 
-# - take unique project name - set it inside the notebooks
-# - bucket creation: remote storage, tifs, earth engine, press, preds-merged
-# - container registry creation
-# env creation
+print("1/5 Parsing arguments")
+openmapflow_str = f"""version: 0.0.1
+project: {args.name}
+description: {args.description}
+gcloud: 
+    project_id: {args.gcloud_project_id}
+    location: {args.gcloud_location}
+    bucket_labeled_tifs: {args.gcloud_bucket_labeled_tifs}
+"""
+print(f"2/5 Writing {CONFIG_FILE}")
+if allow_write(CONFIG_FILE):
+    with open(CONFIG_FILE, "w") as f:
+        f.write(openmapflow_str)
 
-# If project is directly in git repo
-# dvc init
+from openmapflow.config import LIBRARY_DIR, PROJECT_ROOT, DataPaths as dp
 
-# If project is in subdirectory
-# cd <subdir> dvc init --subdir
+print("3/5 Copying over template files")
+for template_file in ["datasets.py", "integration_test_data.py"]:
+    if allow_write(template_file):
+        shutil.copy(str(LIBRARY_DIR / f"template/{template_file}"), str(PROJECT_ROOT))
 
-# Make data directories: [models, features, processed_labels, raw_labels]
+print("4/5 Creating data directories")
+for p in [dp.FEATURES, dp.RAW_LABELS, dp.PROCESSED_LABELS, dp.FEATURES, dp.MODELS]:
+    if allow_write(p):
+        Path(p).mkdir(parents=True, exist_ok=True)
 
-# Add dvc data: [models, features, processed_labels, raw_labels]
-# dvc add data/raw_labels ...
-# dvc commit
+if allow_write(dp.COMPRESSED_FEATURES):
+    with tarfile.open(dp.COMPRESSED_FEATURES, "w:gz") as tar:
+        tar.add(dp.FEATURES, arcname=Path(dp.FEATURES).name)
 
-# Set dvc remote, Google Drive by default for simplicity
-# https://dvc.org/doc/user-guide/setup-google-drive-remote
+print("5/5 Printing dvc instructions")
+dvc_files = " ".join(
+    [dp.RAW_LABELS, dp.PROCESSED_LABELS, dp.COMPRESSED_FEATURES, dp.MODELS]
+)
+print(
+    f"""
+#########################################################################################
+DVC Setup Instructions
+#########################################################################################
+dvc (https://dvc.org/) is used to manage data. To setup run:
 
-# dvc remote add -d gdrive \
-#   gdrive://1EMHILcNFwdusMHHs4eC4OVIJ0Ncp9fiY/crop-mask-example-dvc-store
+# Initializes dvc (use --subdir if in subdirectory)
+dvc init
 
-# https://dvc.org/doc/user-guide/setup-google-drive-remote#authorization
+# Tells dvc to track directories
+dvc add {dvc_files}
 
-# from google.cloud import storage  # type: ignore
+# Connect to remmote storage (we recommend gdrive): https://dvc.org/doc/user-guide/setup-google-drive-remote
+dvc remote add -d gdrive <your google drive folder>
+
+# Push files to remote storage
+dvc push
+"""
+)
