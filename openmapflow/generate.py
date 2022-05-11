b9220775601237e856d752a0d5f268e77ac509f7
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-10 21:45:21 -0400

    Cleaner generation

--- openmapflow/generate.py
+++ openmapflow/generate.py
@@ -6,41 +6,49 @@ from pathlib import Path
 from openmapflow.constants import CONFIG_FILE, LIBRARY_DIR
 
 
-def allow_write(p, force=False):
-    if force or not Path(p).exists():
+def allow_write(p, overwrite=False):
+    if overwrite or not Path(p).exists():
         return True
     p = Path(*Path(p).parts[-4:])
-    overwrite = input(f"{str(p)} already exists. Overwrite? (y/[n]): ")
+    overwrite = input(f"  {str(p)} already exists. Overwrite? (y/[n]): ")
     return overwrite.lower() == "y"
 
 
-def openmapflow_config_from_args(args):
-    return f"""version: 0.0.1
-project: {args.name}
-description: {args.description}
-gcloud:
-    project_id: {args.gcloud_project_id}
-    location: {args.gcloud_location}
-    bucket_labeled_tifs: {args.gcloud_bucket_labeled_tifs}
-"""
+def create_openmapflow_config(overwrite: bool):
+    if not allow_write(CONFIG_FILE, overwrite):
+        return
+    cwd = Path.cwd()
+    project_name = input(f"  Project name [{cwd.stem}]: ") or cwd.stem
+    description = input(f"  Description: ")
+    gcloud_project_id = input(f"  GCloud project ID: ")
+    gcloud_location = input("  GCloud location [us-central1]: ") or "us-central1"
+    gcloud_bucket_labeled_tifs = (
+        input("  GCloud bucket labeled tifs [crop-mask-tifs2]: ") or "crop-mask-tifs2"
+    )
+    openmapflow_str = (
+        f"version: 0.0.1"
+        + f"\nproject: {project_name}"
+        + f"\ndescription: {description}"
+        + "\ngcloud:"
+        + f"\n    project_id: {gcloud_project_id}"
+        + f"\n    location: {gcloud_location}"
+        + f"\n    bucket_labeled_tifs: {gcloud_bucket_labeled_tifs}"
+    )
+
+    with open(CONFIG_FILE, "w") as f:
+        f.write(openmapflow_str)
 
 
-def openmapflow_config_write(openmapflow_str: str, force: bool):
-    if allow_write(CONFIG_FILE, force):
-        with open(CONFIG_FILE, "w") as f:
-            f.write(openmapflow_str)
-
-
-def copy_datasets_py_file(LIBRARY_DIR, PROJECT_ROOT, force: bool):
-    if allow_write("datasets.py", force):
+def copy_datasets_py_file(LIBRARY_DIR, PROJECT_ROOT, overwrite: bool):
+    if allow_write("datasets.py", overwrite):
         shutil.copy(
             str(LIBRARY_DIR / "example_datasets.py"), str(PROJECT_ROOT / "datasets.py")
         )
 
 
-def create_data_dirs(dp, force):
+def create_data_dirs(dp, overwrite):
     for p in [dp.FEATURES, dp.RAW_LABELS, dp.PROCESSED_LABELS, dp.MODELS]:
-        if allow_write(p, force):
+        if allow_write(p, overwrite):
             Path(p).mkdir(parents=True, exist_ok=True)
 
     if allow_write(dp.COMPRESSED_FEATURES):
@@ -58,7 +66,7 @@ def fill_in_action(src_yml_path, dest_yml_path, sub_paths, sub_cd, sub_project):
         f.write(content)
 
 
-def create_github_actions(LIBRARY_DIR, PROJECT_ROOT, PROJECT, dp, force):
+def create_github_actions(LIBRARY_DIR, PROJECT_ROOT, PROJECT, dp, overwrite):
     possible_git_roots = [PROJECT_ROOT, PROJECT_ROOT.parent]
     try:
         git_root = next(r for r in possible_git_roots if (r / ".git").exists())
@@ -73,7 +81,7 @@ def create_github_actions(LIBRARY_DIR, PROJECT_ROOT, PROJECT, dp, force):
     dest_test_yml_path = git_root / f".github/workflows/{PROJECT}-test.yml"
     is_subdir = git_root != PROJECT_ROOT
 
-    if allow_write(dest_deploy_yml_path, force):
+    if allow_write(dest_deploy_yml_path, overwrite):
         fill_in_action(
             src_yml_path=src_deploy_yml_path,
             dest_yml_path=dest_deploy_yml_path,
@@ -82,7 +90,7 @@ def create_github_actions(LIBRARY_DIR, PROJECT_ROOT, PROJECT, dp, force):
             sub_project=PROJECT,
         )
 
-    if allow_write(dest_test_yml_path, force):
+    if allow_write(dest_test_yml_path, overwrite):
         fill_in_action(
             src_yml_path=src_test_yml_path,
             dest_yml_path=dest_test_yml_path,
@@ -120,52 +128,27 @@ jupyter notebook
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate OpenMapFlow project.")
-    parser.add_argument(
-        "--name", type=str, help="Project name", default=Path.cwd().name
-    )
-    parser.add_argument(
-        "--description", type=str, help="Project description", default=""
-    )
-    parser.add_argument(
-        "--gcloud_project_id",
-        type=str,
-        help="Google Cloud Project ID",
-        default="~",
-    )
-    parser.add_argument(
-        "--gcloud_location",
-        type=str,
-        help="Google Cloud Location (default: us-central-1)",
-        default="us-central-1",
-    )
-    parser.add_argument(
-        "--gcloud_bucket_labeled_tifs",
-        type=str,
-        help="Google Cloud Bucket for labeled tifs (default: crop-mask-tifs2)",
-        default="crop-mask-tifs2",
-    )
-    parser.add_argument("--force", action="store_true", help="Force overwrite")
+    parser.add_argument("--overwrite", action="store_true", help="overwrite overwrite")
     args = parser.parse_args()
 
-    print("1/7 Parsing arguments")
-    openmapflow_str = openmapflow_config_from_args(args)
+    n = 6
 
-    print(f"2/7 Writing {CONFIG_FILE}")
-    openmapflow_config_write(openmapflow_str, force=args.force)
+    print(f"1/{n} Writing openmapflow")
+    create_openmapflow_config(args.overwrite)
 
     # Can only import when openmapflow.yaml is available
     from openmapflow.config import PROJECT_ROOT, PROJECT, DataPaths as dp  # noqa E402
 
-    print("3/7 Copying datasets.py file")
-    copy_datasets_py_file(LIBRARY_DIR, PROJECT_ROOT, args.force)
+    print(f"2/{n} Copying datasets.py file")
+    copy_datasets_py_file(LIBRARY_DIR, PROJECT_ROOT, args.overwrite)
 
-    print("4/7 Creating data directories")
-    create_data_dirs(dp=dp, force=args.force)
+    print(f"3/{n} Creating data directories")
+    create_data_dirs(dp=dp, overwrite=args.overwrite)
 
-    print("5/7 Creating Github Actions")
-    create_github_actions(LIBRARY_DIR, PROJECT_ROOT, PROJECT, dp, args.force)
+    print(f"4/{n} Creating Github Actions")
+    create_github_actions(LIBRARY_DIR, PROJECT_ROOT, PROJECT, dp, args.overwrite)
 
-    print("6/7 Printing dvc instructions")
+    print(f"5/{n} Printing dvc instructions")
     dvc_files = [
         dp.RAW_LABELS,
         dp.PROCESSED_LABELS,
@@ -175,5 +158,5 @@ if __name__ == "__main__":
     ]
     print(dvc_instructions.replace("<DVC_FILES>", " ".join(dvc_files)))
 
-    print("7/7 Ready to go!")
+    print(f"6/{n} Ready to go!")
     print(using_openampflow)
