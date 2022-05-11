b6866d36f53b8e2c93ecf60b6517e21c4c04ee96
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-11 10:50:10 -0400

    First unit tests

---
+++ tests/test_processor.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+from datetime import date
+from openmapflow.constants import SUBSET
+from openmapflow.processor import Processor
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append("..")
+
+
+class TestProcessor(TestCase):
+    def test_init(self):
+        kwargs = dict(
+            filename="test.csv", class_prob=0.5, train_val_test=(0.5, 0.5, 0.5)
+        )
+        self.assertRaises(ValueError, lambda: Processor(**kwargs))
+
+    def test_to_date(self):
+        np_date = np.datetime64("2020-01-01")
+        str_date = "2020-01-01"
+        df_date = pd.to_datetime("2020-01-01")
+        obj_date = date(2020, 1, 1)
+        self.assertEqual(Processor._to_date(np_date), obj_date)
+        self.assertEqual(Processor._to_date(str_date), obj_date)
+        self.assertEqual(Processor._to_date(df_date), obj_date)
+
+    def test_train_val_test_split(self):
+        df = pd.DataFrame({"col": list(range(100))})
+
+        df = Processor.train_val_test_split(df, (1.0, 0.0, 0.0))
+        expected_subsets = {"training": 100}
+        self.assertEqual(df[SUBSET].value_counts().to_dict(), expected_subsets)
+
+        df = Processor.train_val_test_split(df, (0.8, 0.1, 0.1))
+        actual_subsets = df[SUBSET].value_counts().to_dict()
+        threshold = 10
+        self.assertTrue(
+            abs(actual_subsets["training"] - 80) < threshold, actual_subsets["training"]
+        )
+        self.assertTrue(
+            abs(actual_subsets["validation"] - 10) < threshold,
+            actual_subsets["validation"],
+        )
+        self.assertTrue(
+            abs(actual_subsets["testing"] - 10) < threshold, actual_subsets["testing"]
+        )
+
+    def test_get_points(self):
+        pass
