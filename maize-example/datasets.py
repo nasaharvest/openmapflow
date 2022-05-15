dbd74a8bc5be5c9c246002dad41f03d573d32288
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-15 03:12:33 -0400

    Updated maize splits

--- maize-example/datasets.py
+++ maize-example/datasets.py
@@ -17,21 +17,32 @@ datasets: List[LabeledDataset] = [
                 class_prob=0,
                 start_year=2019,
                 transform_crs_from=32636,
+                train_val_test=(0.6, 0.2, 0.2),
             ),
             RawLabels(
                 filename="2019_gepro_noncrop.zip",
                 class_prob=0,
                 start_year=2019,
                 transform_crs_from=32636,
+                train_val_test=(0.6, 0.2, 0.2),
             ),
             RawLabels(
-                filename="noncrop_water_kenya_gt.zip", class_prob=0, start_year=2019
+                filename="noncrop_water_kenya_gt.zip",
+                class_prob=0,
+                start_year=2019,
+                train_val_test=(0.6, 0.2, 0.2),
             ),
-            RawLabels(filename="noncrop_kenya_gt.zip", class_prob=0, start_year=2019),
+            RawLabels(
+                filename="noncrop_kenya_gt.zip",
+                class_prob=0,
+                start_year=2019,
+                train_val_test=(0.6, 0.2, 0.2),
+            ),
             RawLabels(
                 filename="kenya_non_crop_test_polygons.zip",
                 class_prob=0,
                 start_year=2019,
+                train_val_test=(0.6, 0.2, 0.2),
             ),
         ),
     ),
@@ -46,6 +57,7 @@ datasets: List[LabeledDataset] = [
                 class_prob=lambda df: df["Crop1"] == "Maize",
                 start_date_col="Planting Date",
                 x_y_from_centroid=False,
+                train_val_test=(0.6, 0.2, 0.2),
             )
             for i in [0, 1, 2]
         ),
