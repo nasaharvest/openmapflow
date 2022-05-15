2201011e6ecbbdfad8d54b6b33d73dca86954740
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-14 20:39:38 -0400

    Add crop type data

--- maize-example/datasets.py
+++ maize-example/datasets.py
@@ -4,25 +4,49 @@ File for storing references to datasets.
 from typing import List
 
 from openmapflow.labeled_dataset import LabeledDataset
-
-# from openmapflow.processor import Processor
+from openmapflow.processor import Processor
 
 datasets: List[LabeledDataset] = [
-    # --------------------------------------------------------------------------
-    # Example LabeledDataset (remove once you have your own)
-    # --------------------------------------------------------------------------
-    # LabeledDataset(
-    #     dataset="example_dataset",
-    #     country="Togo",
-    #     processors=(
-    #         Processor(
-    #             filename="Togo_2019.csv",
-    #             longitude_col="longitude",
-    #             latitude_col="latitude",
-    #             class_prob=lambda df: df["crop"],
-    #             start_year=2019,
-    #             x_y_from_centroid=False,
-    #         ),
-    #     ),
-    # ),
+    LabeledDataset(
+        dataset="Kenya_non_crop_2019",
+        country="Kenya",
+        processors=(
+            Processor(
+                filename="noncrop_labels_v2.zip",
+                class_prob=0,
+                start_year=2019,
+                transform_crs_from=32636,
+            ),
+            Processor(
+                filename="2019_gepro_noncrop.zip",
+                class_prob=0,
+                start_year=2019,
+                transform_crs_from=32636,
+            ),
+            Processor(
+                filename="noncrop_water_kenya_gt.zip", class_prob=0, start_year=2019
+            ),
+            Processor(filename="noncrop_kenya_gt.zip", class_prob=0, start_year=2019),
+            Processor(
+                filename="kenya_non_crop_test_polygons.zip",
+                class_prob=0,
+                start_year=2019,
+            ),
+        ),
+    ),
+    LabeledDataset(
+        dataset="ref_african_crops_kenya_01_labels",
+        country="Kenya",
+        processors=(
+            Processor(
+                filename=f"ref_african_crops_kenya_01_labels_0{i}/labels.geojson",
+                latitude_col="Latitude",
+                longitude_col="Longitude",
+                class_prob=lambda df: df["Crop1"] == "Maize",
+                plant_date_col="Planting Date",
+                x_y_from_centroid=False,
+            )
+            for i in [0, 1, 2]
+        ),
+    ),
 ]
