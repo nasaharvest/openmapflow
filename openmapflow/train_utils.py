956b4ff3fe21557fb4be61516b05d44061af640b
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-05-30 15:08:22 -0400

    Train utils for common functionality

---
+++ openmapflow/train_utils.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+import pandas as pd
+import torch
+
+from openmapflow.config import PROJECT, PROJECT_ROOT, DataPaths
+from openmapflow.constants import COUNTRY, START
+from openmapflow.utils import to_date
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def generate_model_name(val_df: pd.DataFrame, start_month: str) -> str:
+    country = val_df[COUNTRY].iloc[0]
+    start_year = to_date(val_df[START].iloc[0]).year
+    project = PROJECT.replace("-example", "")
+    return f"{country}_{project}_{start_year}_{start_month}"
+
+
+def model_path_from_name(model_name: str) -> Path:
+    return PROJECT_ROOT / DataPaths.MODELS / f"{model_name}.pt"
