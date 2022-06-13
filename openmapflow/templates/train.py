8d7197c9d559ec21172f0f6ac8cc150900534af0
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2022-06-13 15:57:54 +0100

    Use cropharvest.bands.BANDS_MAX to normalize data in train.py

--- openmapflow/templates/train.py
+++ openmapflow/templates/train.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser
 import matplotlib.pyplot as plt
 import pandas as pd
 import torch
+from cropharvest.bands import BANDS_MAX
 from datasets import datasets
 from sklearn.metrics import (
     ConfusionMatrixDisplay,
@@ -84,7 +85,7 @@ class Model(torch.nn.Module):
 
     def forward(self, x):
         with torch.no_grad():
-            x = x * 1e-4  # TODO Fix
+            x = x / torch.tensor(BANDS_MAX)
             x = x.transpose(2, 1)
         x = self.model(x).squeeze(dim=1)
         x = torch.sigmoid(x)
