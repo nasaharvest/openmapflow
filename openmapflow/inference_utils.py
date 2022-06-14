742fe2a00a9e4a25a1959dbbee1e6d8e4655667f
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-06-13 20:00:16 -0400

    Refactor for testing

--- openmapflow/inference_utils.py
+++ openmapflow/inference_utils.py
@@ -4,31 +4,38 @@ from collections import defaultdict
 from glob import glob
 from pathlib import Path
 
+from cropharvest.countries import BBox
 import ee
 from google.cloud import storage
 from tqdm.notebook import tqdm
+from typing import Dict, List, Optional, Tuple
 
 from openmapflow.config import GCLOUD_PROJECT_ID
-from openmapflow.config import BucketNames
 from openmapflow.config import BucketNames as bn
 from openmapflow.labeled_dataset import bbox_from_str
 
-#######################################################
-# Status functions
-#######################################################
-bbox_regex = (
-    r".*min_lat=-?\d*\.?\d*_min_lon=-?\d*\.?\d*_max_lat=-?\d*\.?\d*_max_lon=-?\d*\.?\d*_"
-    + r"dates=\d{4}-\d{2}-\d{2}_\d{4}-\d{2}-\d{2}.*?\/"
-)
 
-
-def get_available_bboxes(buckets_to_check=[BucketNames.INFERENCE_TIFS]):
+def get_available_bboxes(
+    buckets_to_check: List[str] = [bn.INFERENCE_TIFS],
+) -> List[BBox]:
+    """
+    Get all available bboxes from the given buckets using regex.
+    Args:
+        buckets_to_check: List of buckets to check.
+    Returns:
+        List of BBoxes.
+    """
+    if len(buckets_to_check) == 0:
+        raise ValueError("No buckets to check")
     client = storage.Client()
     previous_matches = []
     available_bboxes = []
+    bbox_regex = (
+        r".*min_lat=-?\d*\.?\d*_min_lon=-?\d*\.?\d*_max_lat=-?\d*\.?\d*_max_lon=-?\d*\.?\d*_"
+        + r"dates=\d{4}-\d{2}-\d{2}_\d{4}-\d{2}-\d{2}.*?\/"
+    )
     for bucket_name in buckets_to_check:
-        blobs = client.list_blobs(bucket_or_name=bucket_name)
-        for blob in blobs:
+        for blob in client.list_blobs(bucket_or_name=bucket_name):
             match = re.search(bbox_regex, blob.name)
             if not match:
                 continue
@@ -39,22 +46,39 @@ def get_available_bboxes(buckets_to_check=[BucketNames.INFERENCE_TIFS]):
     return available_bboxes
 
 
-def get_ee_task_amount(prefix=None):
+def get_ee_task_amount(prefix: Optional[str] = None):
+    """
+    Gets amount of active tasks in Earth Engine.
+    Args:
+        prefix: Prefix to filter tasks.
+    Returns:
+        Amount of active tasks.
+    """
     amount = 0
     task_list = ee.data.getTaskList()
     for t in tqdm(task_list):
-        if t["state"] in ["READY", "RUNNING"]:
-            if prefix and prefix in t["description"]:
-                amount += 1
-            else:
-                amount += 1
+        valid_state = t["state"] in ["READY", "RUNNING"]
+        if valid_state and (prefix is None or prefix in t["description"]):
+            amount += 1
     return amount
 
 
-def get_gcs_file_dict_and_amount(bucket_name, prefix):
-    blobs = storage.Client(project=GCLOUD_PROJECT_ID).list_blobs(
-        bucket_name, prefix=prefix
-    )
+def get_gcs_file_amount(
+    bucket_name: str, prefix: str, project: str = GCLOUD_PROJECT_ID
+) -> int:
+    blobs = storage.Client(project=project).list_blobs(bucket_name, prefix=prefix)
+    return len(list(blobs))
+
+
+def get_gcs_file_dict_and_amount(
+    bucket_name: str, prefix: str, project: str = GCLOUD_PROJECT_ID
+) -> Tuple[Dict[str, List[str]], int]:
+    """
+    Gets a dictionary of all files in a bucket with their amount.
+    Returns:
+        Dictionary of files and their amount.
+    """
+    blobs = storage.Client(project=project).list_blobs(bucket_name, prefix=prefix)
     files_dict = defaultdict(lambda: [])
     amount = 0
     for blob in tqdm(blobs, desc=f"From {bucket_name}"):
@@ -64,38 +88,46 @@ def get_gcs_file_dict_and_amount(bucket_name, prefix):
     return files_dict, amount
 
 
-def get_gcs_file_amount(bucket_name, prefix):
-    gcs_files = storage.Client(project=GCLOUD_PROJECT_ID).list_blobs(
-        bucket_name, prefix=prefix
-    )
+def print_between_lines(text: str, line: str = "-", is_tabbed: bool = False):
+    tab = "\t" if is_tabbed else ""
+    print(tab + (line * len(text)))
+    print(tab + text)
+    print(tab + (line * len(text)))
 
-    return len(list(gcs_files))
 
-
-def get_status(model_name_version):
-    print("-----------------------------------------------------------------------")
-    print(model_name_version)
-    print("-----------------------------------------------------------------------")
-    ee_task_amount = get_ee_task_amount(prefix=model_name_version.replace("/", "-"))
-    tifs_amount = get_gcs_file_amount(bn.INFERENCE_TIFS, prefix=model_name_version)
-    predictions_amount = get_gcs_file_amount(bn.PREDS, prefix=model_name_version)
-    print(f"Earth Engine tasks: {ee_task_amount}")
-    print(f"Data available: {tifs_amount}")
-    print(f"Predictions: {predictions_amount}")
+def get_status(prefix: str) -> Tuple[int, int, int]:
+    """
+    Args:
+        prefix: Prefix to filter tasks.
+    Returns:
+        Amount of active tasks, amount of files in Google Cloud storage available for inference,
+        amount of predictions made.
+    """
+    print_between_lines(prefix)
+    ee_task_amount = get_ee_task_amount(prefix=prefix.replace("/", "-"))
+    tifs_amount = get_gcs_file_amount(bn.INFERENCE_TIFS, prefix=prefix)
+    predictions_amount = get_gcs_file_amount(bn.PREDS, prefix=prefix)
+    print(f"1) Obtaining input data: {ee_task_amount}")
+    print(f"2) Input data available: {tifs_amount}")
+    print(f"3) Predictions made: {predictions_amount}")
     return ee_task_amount, tifs_amount, predictions_amount
 
 
-#######################################################
-# Inference functions
-#######################################################
-def find_missing_predictions(model_name_version, verbose=False):
+def find_missing_predictions(
+    prefix: str, verbose: bool = False
+) -> Dict[str, List[str]]:
+    """
+    Finds all missing predictions by data available for
+    inference with the current predictions made
+    Args:
+        prefix: Prefix to filter tasks.
+        verbose: Whether to print the progress.
+    Returns:
+        Dictionary of missing predictions.
+    """
     print("Addressing missing files")
-    tif_files, tif_amount = get_gcs_file_dict_and_amount(
-        bn.INFERENCE_TIFS, prefix=model_name_version
-    )
-    pred_files, pred_amount = get_gcs_file_dict_and_amount(
-        bn.PREDS, prefix=model_name_version
-    )
+    tif_files, tif_amount = get_gcs_file_dict_and_amount(bn.INFERENCE_TIFS, prefix)
+    pred_files, pred_amount = get_gcs_file_dict_and_amount(bn.PREDS, prefix)
     missing = {}
     for full_k in tqdm(tif_files.keys(), desc="Missing files"):
         if full_k not in pred_files:
@@ -107,42 +139,53 @@ def find_missing_predictions(model_name_version, verbose=False):
 
     batches_with_issues = len(missing.keys())
     if verbose:
-        print("-----------------------------------------------------------------------")
-        print(model_name_version)
-        print("-----------------------------------------------------------------------")
-    if batches_with_issues > 0:
-        print(
-            f"\u2716 {batches_with_issues}/{len(tif_files.keys())} "
-            + f"batches have a total {tif_amount - pred_amount} missing predictions"
-        )
-        if verbose:
-            for batch, files in missing.items():
-                print("\t--------------------------------------------------")
-                print(f"\t{Path(batch).stem}: {len(files)}")
-                print("\t--------------------------------------------------")
-                [print(f"\t{f}") for f in files]
-    else:
+        print_between_lines(prefix)
+
+    if batches_with_issues == 0:
         print("\u2714 all files in each batch match")
+        return missing
+
+    print(
+        f"\u2716 {batches_with_issues}/{len(tif_files.keys())} "
+        + f"batches have a total {tif_amount - pred_amount} missing predictions"
+    )
+
+    if verbose:
+        for batch, files in missing.items():
+            print_between_lines(
+                text=f"\t{Path(batch).stem}: {len(files)}", is_tabbed=True
+            )
+            for f in files:
+                print(f"\t{f}")
+
     return missing
 
 
-def make_new_predictions(missing):
-    bucket = storage.Client(project=GCLOUD_PROJECT_ID).bucket(bn.INFERENCE_TIFS)
+def make_new_predictions(
+    missing: Dict[str, List[str]], bucket_name: str = bn.INFERENCE_TIFS
+):
+    """
+    Renames missing files which retriggers inference.
+    Args:
+        missing: Dictionary of missing predictions.
+        bucket_name: Bucket name to rename files in.
+    """
+    bucket = storage.Client(project=GCLOUD_PROJECT_ID).bucket(bucket_name)
     for batch, files in tqdm(missing.items(), desc="Going through batches"):
         for file in tqdm(files, desc="Renaming files", leave=False):
             blob_name = f"{batch}/{file}.tif"
             blob = bucket.blob(blob_name)
             if blob.exists():
-                new_blob_name = f"{batch}/{file}-retry1.tif"
+                new_blob_name = f"{batch}/{file}-retry.tif"
                 bucket.rename_blob(blob, new_blob_name)
             else:
                 print(f"Could not find: {blob_name}")
 
 
-#######################################################
-# Map making functions
-#######################################################
 def gdal_cmd(cmd_type: str, in_file: str, out_file: str, msg=None, print_cmd=False):
+    """
+    Runs a GDAL command: gdalbuildvrt or gdal_translate.
+    """
     if cmd_type == "gdalbuildvrt":
         cmd = f"gdalbuildvrt {out_file} {in_file}"
     elif cmd_type == "gdal_translate":
@@ -157,7 +200,9 @@ def gdal_cmd(cmd_type: str, in_file: str, out_file: str, msg=None, print_cmd=Fal
 
 
 def build_vrt(prefix):
-    # Build vrts for each batch of predictions
+    """
+    Builds a VRT file for each batch and then creates one VRT file for all batches.
+    """
     print("Building vrt for each batch")
     for d in tqdm(glob(f"{prefix}_preds/*/*/")):
         if "batch" not in d:
