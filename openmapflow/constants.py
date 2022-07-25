1820d0fd94c459b63e6adf2047f6d92868d27a47
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-07-25 07:10:58 -0400

    Update version

--- openmapflow/constants.py
+++ openmapflow/constants.py
@@ -12,7 +12,7 @@ TEMPLATE_EVALUATE = TEMPLATES_DIR / "evaluate.py"
 TEMPLATE_REQUIREMENTS = TEMPLATES_DIR / "requirements.txt"
 TEMPLATE_DEPLOY_YML = TEMPLATES_DIR / "github-deploy.yaml"
 TEMPLATE_TEST_YML = TEMPLATES_DIR / "github-test.yaml"
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 # -------------- Dataframe column names --------------------------------------
 SOURCE = "source"
