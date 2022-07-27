ed71130b202ffac09a7caa89640b901b01dfa439
aniket.parlikar316@gmail.com <Aniket-Parlikar>
2022-07-27 13:02:57 -0400

    Added new files for profiling deploy.sh script

--- openmapflow/scripts/deploy.sh
+++ openmapflow/scripts/deploy.sh
@@ -2,9 +2,26 @@
 # This script deploys all the resources necessary to make maps on Google Cloud with OpenMapFlow
 #####################################################################################################
 
+function traceTick(){
+  local CURR_TIME=$(python -c "import time; print(int(time.time()*1000))")
+  if [ -z "$LAST_TIME" ]
+  then
+    START_TIME=$CURR_TIME
+    LAST_TIME=$CURR_TIME
+  fi
+
+  local DELTA=$(($CURR_TIME - $LAST_TIME))
+  local TOTAL_DELTA=$(($CURR_TIME - $START_TIME))
+  LAST_TIME=$CURR_TIME
+
+  printf "Total time elapsed(ms):%-6s Time between current and previous commands(ms):%-6s %s\n" $TOTAL_DELTA $DELTA "$1"
+}
+
+
 set -e # Exit when any command fails
 
-echo "1/8 Setting OpenMapFlow environment variables"
+echo "1/7 Setting OpenMapFlow environment variables"
+traceTick
 export $(
         python -c \
         "from openmapflow.config import deploy_env_variables; \
@@ -13,8 +30,8 @@ export $(
 
 env | grep OPENMAPFLOW
 
-
-echo "2/8 Ensuring latest models are available for deployment"
+echo "2/7 Ensuring latest models are available for deployment"
+traceTick
 dvc pull "$OPENMAPFLOW_MODELS_DIR".dvc -f
 
 export OPENMAPFLOW_MODELS=$(
@@ -24,17 +41,10 @@ export OPENMAPFLOW_MODELS=$(
 )
 echo "MODELS: $OPENMAPFLOW_MODELS"
 
-echo "3/8 Enable Google Cloud APIs: Artifact Registry, Cloud Run, Cloud Functions"
-gcloud services enable artifactregistry.googleapis.com
-gcloud services enable cloudbuild.googleapis.com
-gcloud services enable cloudfunctions.googleapis.com
-gcloud services enable earthengine.googleapis.com
-gcloud services enable run.googleapis.com
-
-
-echo "4/8 Create Google Cloud Buckets if they don't exist"
-for BUCKET in $OPENMAPFLOW_GCLOUD_BUCKET_LABELED_EO \
-        $OPENMAPFLOW_GCLOUD_BUCKET_INFERENCE_EO \
+echo "3/7 Create Google Cloud Buckets if they don't exist"
+traceTick
+for BUCKET in $OPENMAPFLOW_GCLOUD_BUCKET_LABELED_TIFS \
+        $OPENMAPFLOW_GCLOUD_BUCKET_INFERENCE_TIFS \
         $OPENMAPFLOW_GCLOUD_BUCKET_PREDS \
         $OPENMAPFLOW_GCLOUD_BUCKET_PREDS_MERGED
 do
@@ -45,15 +55,17 @@ do
         fi
 done
 
-
-echo "5/8 Checking if Artifact Registry needs to be created for storing OpenMapFlow docker images"
+echo "4/7 Checking if Artifact Registry needs to be created for storing OpenMapFlow docker images"
+traceTick
+gcloud services enable artifactregistry.googleapis.com
 if [ -z "$(gcloud artifacts repositories list --format='get(name)' --filter "$OPENMAPFLOW_PROJECT")" ]; then
         gcloud artifacts repositories create "$OPENMAPFLOW_PROJECT" \
         --location "$OPENMAPFLOW_GCLOUD_LOCATION" \
         --repository-format docker
 fi
 
-echo "6/8 Build and push inference docker image to Google Cloud artifact registry"
+echo "5/7 Build and push inference docker image to Google Cloud artifact registry"
+traceTick
 gcloud auth configure-docker "${OPENMAPFLOW_GCLOUD_LOCATION}"-docker.pkg.dev
 docker build . \
         -f "$OPENMAPFLOW_LIBRARY_DIR"/Dockerfile \
@@ -64,8 +76,8 @@ docker build . \
 
 docker push "$OPENMAPFLOW_DOCKER_TAG"
 
-
-echo "7/8 Deploy inference docker image to Google Cloud Run"
+echo "6/7 Deploy inference docker image to Google Cloud Run"
+traceTick
 echo "Deploying prediction server on port 8080"
 gcloud run deploy "$OPENMAPFLOW_PROJECT" --image "$OPENMAPFLOW_DOCKER_TAG":latest \
         --cpu=4 \
@@ -85,15 +97,15 @@ gcloud run deploy "$OPENMAPFLOW_PROJECT"-management-api --image "$OPENMAPFLOW_DO
         --allow-unauthenticated \
         --port 8081
 
-
-
-echo "8/8 Deploy inference trigger as a Google Cloud Function"
+echo "7. Deploy inference trigger as a Google Cloud Function"
+traceTick
 export OPENMAPFLOW_URL=$(gcloud run services list --platform managed --filter $OPENMAPFLOW_PROJECT --limit 1 --format='get(URL)')
 
 gcloud functions deploy trigger-"$OPENMAPFLOW_PROJECT" \
     --source="$OPENMAPFLOW_LIBRARY_DIR"/trigger_inference_function \
-    --trigger-bucket="$OPENMAPFLOW_GCLOUD_BUCKET_INFERENCE_EO" \
+    --trigger-bucket="$OPENMAPFLOW_GCLOUD_BUCKET_INFERENCE_TIFS" \
     --allow-unauthenticated \
     --runtime=python39 \
     --entry-point=trigger \
-    --set-env-vars MODELS="$OPENMAPFLOW_MODELS",INFERENCE_HOST="$OPENMAPFLOW_URL" 
+    --set-env-vars MODELS="$OPENMAPFLOW_MODELS",INFERENCE_HOST="$OPENMAPFLOW_URL" \
+    --timeout=300s
