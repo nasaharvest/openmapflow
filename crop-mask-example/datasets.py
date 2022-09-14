f8f7577b317f1fe61589af6c508cf0e5f2579d08
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-09-14 13:11:57 -0400

    works for existing datasets

--- crop-mask-example/datasets.py
+++ crop-mask-example/datasets.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from openmapflow.datasets import geowiki_landcover_2017, togo_crop_2019
+from openmapflow.datasets import GeowikiLandcover2017, TogoCrop2019
 from openmapflow.labeled_dataset import LabeledDataset, create_datasets
 
-datasets: List[LabeledDataset] = [geowiki_landcover_2017, togo_crop_2019]
+datasets: List[LabeledDataset] = [GeowikiLandcover2017(), TogoCrop2019()]
 
 if __name__ == "__main__":
     create_datasets(datasets)
--- openmapflow/datasets.py
+++ openmapflow/datasets.py
@@ -1,41 +1,22 @@
-from openmapflow.labeled_dataset_existing import ExistingLabeledDataset
+from openmapflow.labeled_dataset import LabeledDataset
 
-geowiki_landcover_2017 = ExistingLabeledDataset(
-    dataset="geowiki_landcover_2017",
-    source=(
-        "Linda See. A global reference database of crowdsourced cropland data collected using the "
-        + "Geo-Wiki platform, 2017."
-    ),
-    label_type="binary",
-    license="CC BY-3.0",
-    country="global",
-    download_url="https://storage.googleapis.com/harvest-public-assets/openmapflow/datasets/crop/geowiki_landcover_2017.csv",
-    source_code_url="https://github.com/nasaharvest/crop-mask/blob/master/datasets.py",
-)
+import pandas as pd
 
-togo_crop_2019 = ExistingLabeledDataset(
-    country="Togo",
-    dataset="Togo_2019",
-    download_url="https://storage.googleapis.com/harvest-public-assets/openmapflow/datasets/crop/Togo_2019.csv",
-    label_type="binary",
-    license="CC BY-4.0",
-    source=(
-        "Hannah Kerner, Gabriel Tseng, Inbal Becker-Reshef, Catherine Nakalembe, "
-        + " Brian Barker, Blake Munshell, Madhava Paliyam, and Mehdi Hosseini. Rapid response "
-        + "crop maps in data sparse regions. In ACM SIGKDD Conference on Data Mining and "
-        + "Knowledge Discovery Workshops, 2020."
-    ),
-    source_code_url="https://github.com/nasaharvest/crop-mask/blob/master/datasets.py",
-)
+gcloud_url = "https://storage.googleapis.com/harvest-public-assets/openmapflow/datasets"
 
-kenya_crop_2018_2019 = ExistingLabeledDataset(
-    country="Kenya",
-    dataset="Kenya_2018_2019",
-    label_type="binary",
-    license="CC BY-4.0",
-    download_url="https://storage.googleapis.com/harvest-public-assets/openmapflow/datasets/crop/Kenya_2018_2019.csv",
-    source_code_url="https://github.com/nasaharvest/crop-mask/blob/master/datasets.py",
-)
+class GeowikiLandcover2017(LabeledDataset):
+    """Source: https://github.com/nasaharvest/crop-mask/blob/master/datasets.py"""
+    def load_labels(self):
+        return pd.read_csv(f"{gcloud_url}/crop/geowiki_landcover_2017.csv")
+            
+class TogoCrop2019(LabeledDataset):
+    """Source: https://github.com/nasaharvest/crop-mask/blob/master/datasets.py"""
+    def load_labels(self):
+        return pd.read_csv(f"{gcloud_url}/crop/Togo_2019.csv")
 
+class KenyaCrop201819(LabeledDataset):
+    """Source: https://github.com/nasaharvest/crop-mask/blob/master/datasets.py"""
+    def load_labels(self):
+        return pd.read_csv(f"{gcloud_url}/crop/Kenya_2018_2019.csv")
 
-datasets = [geowiki_landcover_2017, togo_crop_2019, kenya_crop_2018_2019]
+datasets = [GeowikiLandcover2017(), TogoCrop2019(), KenyaCrop201819()]
