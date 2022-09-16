211690b70c9930b55b40390cb897de06e6581f08
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-09-16 11:12:16 -0400

    Make new template

--- openmapflow/templates/datasets.py
+++ openmapflow/templates/datasets.py
@@ -3,37 +3,45 @@ File for storing references to datasets.
 """
 from typing import List
 
-# from openmapflow.datasets import geowiki_landcover_2017
+from datetime import date
+
+import pandas as pd
+
+from openmapflow.constants import CLASS_PROB, LAT, LON, START, END, COUNTRY, SUBSET
+from openmapflow.config import PROJECT_ROOT, DataPaths
+from openmapflow.datasets import GeowikiLandcover2017
 from openmapflow.labeled_dataset import LabeledDataset, create_datasets
+from openmapflow.label_utils import train_val_test_split
 
-# from openmapflow.labeled_dataset_custom import CustomLabeledDataset
-# from openmapflow.raw_labels import RawLabels
+label_col = CLASS_PROB
 
-datasets: List[LabeledDataset] = [
-    # --------------------------------------------------------------------------
-    # Example ExistingLabeledDataset
-    # --------------------------------------------------------------------------
-    # geowiki_landcover_2017,
-    #
-    #
-    # --------------------------------------------------------------------------
-    # Example CustomLabeledDataset
-    # --------------------------------------------------------------------------
-    # CustomLabeledDataset(
-    #     dataset="example_dataset",
-    #     country="Togo",
-    #     raw_labels=(
-    #         RawLabels(
-    #             filename="Togo_2019.csv",
-    #             longitude_col="longitude",
-    #             latitude_col="latitude",
-    #             class_prob=lambda df: df["crop"],
-    #             start_year=2019,
-    #             x_y_from_centroid=False,
-    #         ),
-    #     ),
-    # ),
-]
+# -----------------------------------------------------------------------------
+# Example custom dataset to be used as reference
+# -----------------------------------------------------------------------------
+class TogoCrop2019(LabeledDataset):
+    def load_labels(self) -> pd.DataFrame:
+        # Read in raw label file
+        df = pd.read_csv(PROJECT_ROOT / DataPaths.RAW_LABELS / "Togo_2019.csv")
+
+        # Rename coordinate columns to be used for getting Earth observation data
+        df.rename(columns={"latitude": LAT, "longitude": LON}, inplace=True)
+
+        # Set start and end date for Earth observation data
+        df[START], df[END] = date(2019, 1, 1), date(2020, 12, 31)
+
+        # Set consistent label column
+        df[label_col] = df["crop"].astype(float)
+
+        # Split labels into train, validation, and test sets
+        df[SUBSET] = train_val_test_split(index=df.index, val=0.2, test=0.2)
+
+        # Set country column for later analysis
+        df[COUNTRY] = "Togo"
+
+        return df
+
+
+datasets: List[LabeledDataset] = [GeowikiLandcover2017(), TogoCrop2019()]
 
 if __name__ == "__main__":
     create_datasets(datasets)
