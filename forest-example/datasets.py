7ec09a4ddb19a32fbb28007a369ab4444765e149
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-09-19 12:38:14 -0400

    Process 1000 points

--- forest-example/datasets.py
+++ forest-example/datasets.py
@@ -8,7 +8,6 @@ import pandas as pd
 
 from openmapflow.config import PROJECT_ROOT, DataPaths
 from openmapflow.constants import COUNTRY, END, LAT, LON, START, SUBSET
-from openmapflow.datasets import GeowikiLandcover2017
 from openmapflow.label_utils import train_val_test_split
 from openmapflow.labeled_dataset import LabeledDataset, create_datasets
 
@@ -21,27 +20,31 @@ label_col = "is_forest"
 class UgandaForest2019(LabeledDataset):
     def load_labels(self) -> pd.DataFrame:
         # Read in raw label file
-        df = pd.read_csv(PROJECT_ROOT / DataPaths.RAW_LABELS / "Togo_2019.csv")
+        df = pd.read_csv(
+            PROJECT_ROOT / DataPaths.RAW_LABELS / "hansen_labelled_data.csv"
+        )
+
+        df = df.sample(n=1000, random_state=42)
 
         # Rename coordinate columns to be used for getting Earth observation data
-        df.rename(columns={"latitude": LAT, "longitude": LON}, inplace=True)
+        df.rename(columns={"lon": LAT, "lat": LON}, inplace=True)
 
         # Set start and end date for Earth observation data
         df[START], df[END] = date(2019, 1, 1), date(2020, 12, 31)
 
         # Set consistent label column
-        df[label_col] = df["crop"].astype(float)
+        df[label_col] = (df["label"] == "Forest").astype(float)
 
         # Split labels into train, validation, and test sets
         df[SUBSET] = train_val_test_split(index=df.index, val=0.2, test=0.2)
 
         # Set country column for later analysis
-        df[COUNTRY] = "Togo"
+        df[COUNTRY] = "Uganda"
 
         return df
 
 
-datasets: List[LabeledDataset] = [GeowikiLandcover2017(), TogoCrop2019()]
+datasets: List[LabeledDataset] = [UgandaForest2019()]
 
 if __name__ == "__main__":
     create_datasets(datasets)
