2ed8b67c85fa4229d4cfa09b80b580be10b87e59
Adebowale <dansy37@gmail.com>
2022-10-18 20:51:11 +0200

    to_polygon method

--- openmapflow/ee_adminboundary.py
+++ openmapflow/ee_adminboundary.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 import ee
 import numpy as np
@@ -23,8 +24,30 @@ class EEAdminBoundary(AdminBoundary):
             else:
                 return ee.Feature(ee.Geometry.Polygon(cords))
 
-    # TODO: divide the polygon into smaller polygons
-    # def to_polygons(self) -> List[ee.Geometry.Polygon]:
-    #     pass
+    def to_polygon(self, size_per_patch: int = 3300) -> List[ee.Geometry.Polygon]:
 
-    # def from_center
+        self.boundary = self.boundary.to_crs(
+            epsg=3857
+        )  # TODO: might need a second on this
+        xmin, ymin, xmax, ymax = self.boundary.total_bounds
+
+        cols = np.arange(xmin, xmax + size_per_patch, size_per_patch)
+        rows = np.arange(ymin, ymax + size_per_patch, size_per_patch)
+
+        print(f"Splitting into {len(cols)-1} columns and {len(rows)-1} rows")
+
+        out_poygons: List[ee.Geometry.Polygon] = []
+        for x in cols[:-1]:
+            for y in rows[:-1]:
+                out_poygons.append(
+                    ee.Geometry.Polygon(
+                        [
+                            [x, y],
+                            [x, y + size_per_patch],
+                            [x + size_per_patch, y + size_per_patch],
+                            [x + size_per_patch, y],
+                        ],
+                    )
+                )
+
+        return out_poygons
