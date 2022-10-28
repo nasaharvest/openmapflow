17cb05028333b1c6e5217ff9eea91b77bff59e85
Adebowale <dansy37@gmail.com>
2022-10-28 04:51:33 +0200

    Add test admin bounds

---
+++ tests/test_admin_bounds.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from openmapflow.admin_bounds import AdminBoundary
+
+
+class TestAdminBound(TestCase):
+    def test_adminbound_instance(self):
+        admin_bound = AdminBoundary(country_iso3="RWA", regions_of_interest=[])
+        self.assertEqual(
+            admin_bound.boundary.geom_type[0], "Polygon")
