ff12b6b68fa8e2e772a3563ef1aadccdd85693d7
Adebowale <dansy37@gmail.com>
2022-11-02 16:16:54 +0100

    black fix

--- tests/test_admin_bounds.py
+++ tests/test_admin_bounds.py
@@ -6,5 +6,4 @@ from openmapflow.admin_bounds import AdminBoundary
 class TestAdminBound(TestCase):
     def test_adminbound_instance(self):
         admin_bound = AdminBoundary(country_iso3="RWA", regions_of_interest=[])
-        self.assertEqual(
-            admin_bound.boundary.geom_type[0], "Polygon")
+        self.assertEqual(admin_bound.boundary.geom_type[0], "Polygon")
