ed968e6f4c6e33da9baf06b7ceb6b3fb1b5a875e
ivanzvonkov <ivan.zvonkov@gmail.com>
2023-02-12 14:06:26 -0500

    Update public datasets

--- openmapflow/datasets.py
+++ openmapflow/datasets.py
@@ -3,7 +3,7 @@ import pandas as pd
 from openmapflow.constants import CLASS_PROB
 from openmapflow.labeled_dataset import LabeledDataset
 
-gcloud_url = "https://storage.googleapis.com/harvest-public-assets/openmapflow/datasets"
+gcloud_url = "https://storage.googleapis.com/openmapflow-public/datasets"
 
 label_col = CLASS_PROB
 
@@ -11,7 +11,7 @@ label_col = CLASS_PROB
 class GeowikiLandcover2017(LabeledDataset):
     def load_labels(self):
         # Source: https://github.com/nasaharvest/crop-mask/blob/master/datasets.py
-        df = pd.read_csv(f"{gcloud_url}/crop/geowiki_landcover_2017.csv")
+        df = pd.read_csv(f"{gcloud_url}/crop/GeowikiLandcover2017.csv")
         df = df[df[label_col] != 0.5].copy()
         return df
 
@@ -19,7 +19,7 @@ class GeowikiLandcover2017(LabeledDataset):
 class TogoCrop2019(LabeledDataset):
     def load_labels(self):
         # Source: https://github.com/nasaharvest/crop-mask/blob/master/datasets.py
-        df = pd.read_csv(f"{gcloud_url}/crop/Togo_2019.csv")
+        df = pd.read_csv(f"{gcloud_url}/crop/TogoCrop2019.csv")
         df = df[df[label_col] != 0.5].copy()
         return df
 
@@ -30,4 +30,4 @@ class KenyaCrop201819(LabeledDataset):
         return pd.read_csv(f"{gcloud_url}/crop/Kenya_2018_2019.csv")
 
 
-datasets = [GeowikiLandcover2017(), TogoCrop2019(), KenyaCrop201819()]
+datasets = [GeowikiLandcover2017(), TogoCrop2019()]
